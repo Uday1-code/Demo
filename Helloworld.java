a41a7faf6cc5af670d13e217b703140155090951
uday pspk <udaykumar90599@gmail.com>
2020-10-15 19:03:40 +0530

    1st file eidted

--- Helloworld.java
+++ Helloworld.java
@@ -3,8 +3,6 @@ class Helloworld
 public static void main{String[] args}
 {
 	System.out.println{"hello world"};
-	System.out.println{"edit on remote"};
-	 System.out.println{"edit on local"};
 
 }
 
